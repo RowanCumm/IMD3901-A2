b3ae5150780ceec1f0f1235f2520aece3ce3871e
RowanCumm <59933474+RowanCumm@users.noreply.github.com>
2020-01-25 17:25:45 -0500

    button delet old models create new

--- public/js/create-cow-component.js
+++ public/js/create-cow-component.js
@@ -4,10 +4,16 @@ AFRAME.registerComponent( 'create-cow-component',{
         console.log('init component');
 
         const Context_AF = this;
-
+        const skyElem = document.querySelector("#background");
         Context_AF.el.addEventListener('click', function(event) {
             console.log('click');
-
+            if (Context_AF.el.getAttribute('id') === 'button1' && (skyElem.getAttribute('src') != '#sky1')
+            ||Context_AF.el.getAttribute('id') === 'button2' && (skyElem.getAttribute('src') != '#sky2')
+            || Context_AF.el.getAttribute('id') === 'button3' && (skyElem.getAttribute('src') != '#sky3'))
+            {
+                Context_AF.deleteExisting();
+            }
+            
             Context_AF.createCow();
         });
 
@@ -20,26 +26,61 @@ AFRAME.registerComponent( 'create-cow-component',{
         });
 
     },
+    deleteExisting: function(){
+
+        var sceneEl = document.querySelector('a-scene');
+
+        document.querySelectorAll(".bois").forEach(e => e.parentNode.removeChild(e));
+       
+    },
     createCow: function() {
         const Context_AF = this;
 
+        let modelElem = document.createElement('a-entity');
+        modelElem.setAttribute('class','clickable bois');
+        modelElem.setAttribute('id', 'bois');
+
+
+        
         const skyElem = document.querySelector("#background");
 
         if (Context_AF.el.getAttribute('id') === 'button1')
             {
-                skyElem.setAttribute('src', '#sky1');
+                if(skyElem.getAttribute('src') != '#sky1'){
+                    skyElem.setAttribute('src', '#sky1');
+                }
+                
+                modelElem.setAttribute('obj-model', 'obj: #appleObj');
+                modelElem.setAttribute('material', 'color: #FF0000');
+                modelElem.setAttribute('scale', {x:0.1, y:0.1, z:0.1});
             }
         else if (Context_AF.el.getAttribute('id') === 'button2')
             {
-
-                skyElem.setAttribute('src', '#sky2');
+                if(skyElem.getAttribute('src') != '#sky2'){
+                    skyElem.setAttribute('src', '#sky2');
+                }
+                modelElem.setAttribute('obj-model', 'obj: #pizzaObj');
+                modelElem.setAttribute('material', 'color: #F56826');
+                modelElem.setAttribute('scale', {x:4, y:4, z:4});
             }
         else if (Context_AF.el.getAttribute('id') === 'button3')
             {
-
-                skyElem.setAttribute('src', '#sky3');
+                if(skyElem.getAttribute('src') != '#sky3'){
+                    skyElem.setAttribute('src', '#sky3');
+                }
+                modelElem.setAttribute('obj-model', 'obj: #rexObj');
+                modelElem.setAttribute('scale', {x:0.025, y:0.025, z:0.025});
+                modelElem.setAttribute('material', 'color: #19992A');
             }
-         
+        
+        
+        modelElem.setAttribute('delete-cow-component', '');
+        modelElem.setAttribute('position', { x:(Math.random() * 12.0) -4.0, y:0, z:-15});
+        
+        modelElem.setAttribute('rotation', {x:0, y:Math.random() * 360.0, z:0});
+
+        let scene = document.querySelector('a-scene');
+        scene.appendChild(modelElem);
         
     }
 });
